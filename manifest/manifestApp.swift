548122b0c9508e6cff09e9a30953a78d34881021
Ivan <ivantaiwan4@gmail.com>
2026-02-23 21:43:53 +0800

    Initial Commit

---
+++ manifest/manifestApp.swift
@@ -0,0 +1,17 @@
+//
+//  manifestApp.swift
+//  manifest
+//
+//  Created by Ivan Hsieh on 2026/2/23.
+//
+
+import SwiftUI
+
+@main
+struct manifestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
